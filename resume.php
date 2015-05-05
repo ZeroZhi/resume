30781865c8ec53884cafdaf3609803e55b50f980
ZeroZhi <zekazilin@gmail.com>
2015-05-05 13:34:23 +0300

    okay php

---
+++ resume.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+	<title>Резюме</title>
+  <style>
+   body {
+    margin: 70;
+   }
+   img {
+    border: 7px solid #cccccc;
+   }
+  </style>
+</head>
+<body><img src="f11.jpg" align="right" width="120" border="5">
+<h3>Жилин Евгений Юрьевич</h3>
+<table margin="50">
+ <tr>
+  <td height="15" width="150">Дата рождения:</td>
+  <td>21 окт 1983 (31 год)</td>
+ </tr>
+ <tr>
+  <td height="15">Регион:</td>
+  <td>Харьков</td>
+ </tr>
+ <tr>
+  <td height="15">Телефон(ы):</td>
+  <td>093 140 92 87</td>
+ </tr>
+ <tr>
+  <td height="15">E-mail:</td>
+  <td>zekazilin@gmail.com</td>
+ </tr>
+</table><br><br><br>
+<h1><center>Junior PHP developer</center></h1><br><br><br>
+<font size="+2" color="#003366">Образование</font>
+<hr align="left" width="825" size="2">
+Среднее<br><br><br>
+<font size="+2" color="#003366">Ключевая информация</font>
+<hr align="left" width="825" size="2">
+Ответственность, пунктуальность. Легкообучаем.<br><br><br>
+<font size="+2" color="#003366">Опыт работы</font>
+<hr align="left" width="825" size="2">
+<ul><li><font size="+1"><b>Оператор видеонаблюдения</b></font><br>
+мар 2008 - июн 2013 (5 лет 4 мес)<br>
+<font color="#333333"><b>Casino</b></font><br>
+<font size="-1" color="#999999">Охрана и Безопасность</font></li><br>
+........................................................................................................................................<br>
+<li><font size="+1"><b>Оператор цифровой фотолаборатории</b></font><br>
+июл 2002 - май 2007 (4 года 11 мес)<br>
+<font color="#333333"><b>Royal Foto Center</b></font><br>
+<font size="-1" color="#999999">Промышленность и Производство</font></li></ul><br><br><br>
+<font size="+2" color="#003366">Курсы, тренинги, сертификаты</font>
+<hr align="left" width="825" size="2">
+<ul><li><font size="+1" color="#333333"><b>Курсы PHP</b></font><br>
+<font size="-1" color="#999999">Год окончания 2015</font><br>
+<b>В процессе обучения.</b></li></ul><br><br><br>
+<font size="+2" color="#003366">Дополнительная информация</font>
+<hr align="left" width="825" size="2">
+<ul><li><font size="+1" color="#333333"><b>Водительское удостоверение</b></font><br>
+B.C.</li><br>
+........................................................................................................................................<br>
+<li><font size="+1" color="#333333"><b>Увлечения и интересы</b></font><br>
+Велосипед, бильярд, физ-ра.</li></ul>
+</body>
+</html>
